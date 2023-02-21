3bde5d5e1029c350c979c9d30e4e1792f0c8309e
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2023-02-22 00:45:05 +0530

    Add Reset Password UI

---
+++ app/src/main/java/ashutosh/healthhive/ui/auth/forgotPasswordOtpVerification/ForgotPasswordOtpVerificationViewModel.kt
@@ -0,0 +1,7 @@
+package ashutosh.healthhive.ui.auth.forgotPasswordOtpVerification
+
+import androidx.lifecycle.ViewModel
+
+class ForgotPasswordOtpVerificationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
